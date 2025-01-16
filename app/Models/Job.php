00691aea0957d5d3213707ccbef233d4d598458b
Asim <asimsaeed353@gmail.com>
2025-01-16 16:46:11 +0500

    Day 10
    Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Job extends Model {
+
+    use HasFactory;  // to generate factories of jobs
+
     protected $table = 'job_listings';  // database table name
     protected $fillable = ['title', 'salary'];  // explicitly allowing attributes to be mass assigned
 }
