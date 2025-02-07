7996a45b2a107a68b966ca94e65e53becb1f5f42
Asim <asimsaeed353@gmail.com>
2025-02-07 21:09:36 +0500

    Complete 26 Lesson
    Bundling assets and Vite

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/js/app.js'])
     <title>Laravel</title>
 </head>
 <body class="h-full">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
@@ -14,6 +12,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "laracasts": "rgb(150,100,50)",
+            }
         },
     },
     plugins: [],
